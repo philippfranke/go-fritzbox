27d5ab986064a98f073f3be8b5c51dbf42c674bc
Philipp Franke <dev@philippfranke.com>
2015-01-29 02:03:52 +0100

    gofmt -s, travis-CI

--- fritzbox/session_test.go
+++ fritzbox/session_test.go
@@ -1,3 +1,8 @@
+// Copyright 2015 The go-fritzbox AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package fritzbox
 
 import (
